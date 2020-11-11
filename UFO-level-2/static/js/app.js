117a2fd021f0f987f5d1e59dffc4840a4f9dffe4
AnjanaDelhi <46753092+AnjanaDelhi@users.noreply.github.com>
2020-11-10 19:38:38 -0600

    Level 2

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,61 @@
+// from data.js
+var tableData = data;
+
+
+//Use D3 to select the table body
+var tbody = d3.select("tbody");
+
+// Loop through array of objects then each object
+tableData.forEach((item) => {
+  console.log(item);
+  var row = tbody.append("tr");
+
+// Get the entries for each object in the array
+Object.entries(item).forEach(([key, value]) => {
+    // Log the key and value
+    console.log(`Key: ${key} and Value ${value}`);
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+// Select the filterdate button
+var filterDate = d3.select("#filter-btn");
+
+filterDate.on("click", function() {
+  // Select the input date
+  var inputDate = d3.select("#datetime" || "#city")
+  //||
+  // d3.select("#city")||
+   // d3.select("#state")||
+  //d3.select("#country")||
+  // d3.select("#shape");
+  var inputDateValue = inputDate.property("value");
+
+
+  var filterTable = tableData.filter(item => item.datetime === inputDateValue||
+    item.city === inputDateValue || 
+    item.state === inputDateValue||
+    item.country === inputDateValue||
+    item.shape === inputDateValue);
+
+  // remove any children from the list to
+  tbody.html("");
+
+  //repopulate with the filtered data
+// Loop through array of objects then each object
+  filterTable.forEach((item) => {
+    console.log(item);
+    var row = tbody.append("tr");
+
+  // Get the entries for each object in the array
+  Object.entries(item).forEach(([key, value]) => {
+      // Log the key and value
+      console.log(`Key: ${key} and Value ${value}`);
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+})
+
